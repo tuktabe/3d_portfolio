6d6f63047b90db66515267ba3dd282e8a156cb22
Tukta Belding <tukta.belding@outlook.com>
2024-10-18 14:26:38 -0700

    fix port to environment or 8080

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: process.env.PORT || 8080,  // Default to 8080 or the environment-provided port
+    host: true  // This allows the server to be accessible externally
+  }
 })
